69d721b0b14701fef233cbb1a59dcd064291906c
Emil Helge Knochenhauer Jürgensen <emil428f@edu.ucl.dk>
2021-11-10 11:19:57 +0100

    Add project files.

---
+++ API_Test/ConnectionTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace API_Test
+{
+    public class ConnectionTests
+    {
+        [Fact]
+        public void API_Can_Connect_To_Data_Source()
+        {
+
+        }
+    }
+}
---
+++ Microservice/Controllers/TrailerController.cs
@@ -0,0 +1,61 @@
+﻿using Microservice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrailerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Trailer> Get()
+        {
+            return new List<Trailer>()
+            {
+                new Trailer()
+                {
+                    Name = "Test",
+                    Description = "Test",
+                    Price = 9.99M,
+                    CategoryId = 1
+                }
+            };
+        }
+
+        [HttpGet("{id}")]
+        public Trailer Get(int? id)
+        {
+            return
+                new Trailer()
+                {
+                    Name = "Test",
+                    Description = "Test",
+                    Price = 9.99M,
+                    CategoryId = 1
+                };
+        }
+
+        [HttpPost]
+        public Trailer Post([FromBody] Trailer product)
+        {
+            return product;
+        }
+
+        // No need to update GPS location.
+        [HttpPut("{id}")]
+        public Trailer Put(int? id, [FromBody] Trailer product)
+        {
+            return product;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int? id)
+        {
+
+        }
+    }
+}
