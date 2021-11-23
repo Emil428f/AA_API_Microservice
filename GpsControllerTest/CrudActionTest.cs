e3ecbe3d1231cf9f49062aca8c2dcc20ca7a7ac3
Julian Petersen <juli11c0@edu.ucl.dk>
2021-11-23 15:04:54 +0100

    added test suite and nuget packages

---
+++ GpsControllerTest/CrudActionTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+namespace GpsRepositoryTest
+{
+    public class CrudActionTest
+    {
+        public CrudActionTest()
+        {
+
+        }
+
+        [Fact]
+        public void GetAll_ListOfGpsCoordinates_ReturnList()
+        {
+            throw new NotFiniteNumberException();
+        }
+    }
+}
