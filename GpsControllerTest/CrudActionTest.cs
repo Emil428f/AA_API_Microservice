895271fbf1b976723b86bc05b534da91540a4656
Julian Petersen <juli11c0@edu.ucl.dk>
2021-11-25 08:29:25 +0100

    save

--- GpsControllerTest/CrudActionTest.cs
+++ GpsControllerTest/CrudActionTest.cs
@@ -5,21 +5,33 @@ using FluentAssertions;
 using WebAPI.Interfaces;
 using WebAPI.EntityFramework.Repositories;
 using WebAPI.EntityFramework.Context;
+using WebAPI.EntityFramework.Repositories.Abstract;
+using WebAPI.Models;
+using WebAPI.Interfaces.Base;
 
 namespace GpsRepositoryTest
 {
     public class CrudActionTest
     {
-        public Mock<IGpsRepository> _igpsRepo;
+        //public Mock<IGpsRepository> _igpsRepo;
+        public Mock<IGps> _igps;
+        //public Mock<IGenericRepository<IGps>> _igeneric;
+        public FakeGpsContext _context;
 
         public CrudActionTest()
         {
-            _igpsRepo = new Mock<IGpsRepository>();
+            //_igpsRepo = new Mock<IGpsRepository>();
+            //_igeneric = new Mock<IGenericRepository<IGps>>();
+            _igps = new Mock<IGps>();
+            _context = new FakeGpsContext();
         }
 
         [Fact]
         public void GetAll_ListOfGpsCoordinates_ReturnList()
         {
+            //GpsRepository repo = new GpsRepository(_context);
+
+            IGenericRepository<IGps> generic = new IGenericRepository<Gps>();
             throw new NotImplementedException();
         }
     }
---
+++ WebAPI/Interfaces/IGps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Interfaces
+{
+    public interface IGps
+    {
+        public int Id { get; set; }
+
+        public string Coordinates { get; set; }
+    }
+}
